49381d6f194c8463fb7b6a857c6010c1d4383297
AliAgdas <aliagdas03@gmail.com>
2024-08-26 11:04:30 +0300

    first

---
+++ src/main/java/org/example/soapproject/config/SoapConfig.java
@@ -0,0 +1,28 @@
+package org.example.soapproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+
+@Configuration
+public class SoapConfig {
+
+    @Bean
+    public Jaxb2Marshaller marshaller() {
+        final Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setContextPath("com.wcf.wsdl");
+        return marshaller;
+    }
+
+    @Bean
+    public CalculatorClient calculatorclient(Jaxb2Marshaller marshaller) {
+        final CalculatorClient client = new CalculatorClient();
+        client.setDefaultUri("http://www.dneonline.com/calculator.asmx");
+        client.setMarshaller(marshaller);
+        client.setUnmarshaller(marshaller);
+        return client;
+    }
+
+}
+
---
+++ src/test/java/org/example/soapproject/SoapProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.soapproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
